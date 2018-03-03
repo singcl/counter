b91cc4c964ad56a630d456a392d245dd329de614
singcl <24661881@qq.com>
2018-03-03 16:41:40 +0800

    :art: 删除示例组件，添加业务组件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import Counter from './components/Counter'
+import counter from './reducers'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App name="SINGCL" />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(counter)
+const rootEl = document.getElementById('root')
+
+const render = () => ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    />, 
+    rootEl
+)
+
+render()
+store.subscribe(render)
+registerServiceWorker()
